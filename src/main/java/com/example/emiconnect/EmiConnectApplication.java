845d85292ed1ce7344e14716c82c0db6fe6dabeb
Anasa <hamidlmath534@gmail.com>
2022-12-09 16:03:03 +0000

    Initial commit

---
+++ src/main/java/com/example/emiconnect/EmiConnectApplication.java
@@ -0,0 +1,13 @@
+package com.example.emiconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmiConnectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmiConnectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/emiconnect/EmiConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.emiconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmiConnectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
